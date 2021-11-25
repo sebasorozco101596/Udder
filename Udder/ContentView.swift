b92454a7ac265edfb9581bf5ca978e03ebc9e11a
Juan S Orozco Buitrago <Sebasorozcob@gmail.com>
2021-11-25 12:29:09 -0500

    Adding the new button

--- Udder/ContentView.swift
+++ Udder/ContentView.swift
@@ -16,6 +16,9 @@ struct ContentView: View {
                 Text("Fresh milk on demand")
                     .font(.system(size: 20))
                 Spacer()
+                Button("TODO") {
+                    
+                }
                 
             }
         }
